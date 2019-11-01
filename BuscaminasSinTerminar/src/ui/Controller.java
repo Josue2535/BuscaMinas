f8dfccbccd5c441d0b51bd67a95c6b750e15e4ea
Josue Rodriguez Pineda <1005969243@academico.icesi.edu.co>
2019-11-01 18:46:27 -0500

    yaaaa

--- BuscaminasSinTerminar/src/ui/Controller.java
+++ BuscaminasSinTerminar/src/ui/Controller.java
@@ -13,31 +13,39 @@ import modelo.Buscaminas;
 public class Controller {
 	private Buscaminas bu;
 	public void level(Button b) {
-		if(b.getText().equalsIgnoreCase("Principiante")) {
-			b.setOnAction(e->{createLevel( bu.FILAS_PRINCIPIANTE, bu.COLUMNAS_PRINCIPIANTE);
-			bu = new Buscaminas(1);});
+		if(b.getText().equalsIgnoreCase("PRINCIPIANTE")) {
+			b.setOnAction(e->{bu = new Buscaminas(1);
+			createLevel(bu.COLUMNAS_PRINCIPIANTE, bu.FILAS_PRINCIPIANTE);});
 		}
-		if(b.getText().equalsIgnoreCase("Intermedio")) {
-			b.setOnAction(e->{createLevel( 8, 8);
-			bu = new Buscaminas(2);});
+		else if(b.getText().equalsIgnoreCase("INTERMEDIO")) {
+			b.setOnAction(e->{bu = new Buscaminas(2);
+			createLevel( bu.FILAS_INTERMEDIO, bu.COLUMNAS_INTERMEDIO);});
 		}
+		else if(b.getText().equalsIgnoreCase("EXPERTO")) {
+			b.setOnAction(e->{bu = new Buscaminas(3);createLevel(bu.FILAS_EXPERTO, bu.COLUMNAS_EXPERTO);});
+		}
+		
 	}
 	
 	public void createLevel( int i, int j) {
 		Pane root = new Pane();
 		VBox vbox = new VBox();
 		GridPane gp = new GridPane();
+		GridPane gp1 = new GridPane();
 		for (int k = 0; k < i; k++) {
 			for (int k2 = 0; k2 < j; k2++) {
 				Button b1 = new Button(bu.darCasillas()[k][k2].mostrarValorCasilla());
 				b1.setId("k,k2");
 				b1.setOnAction(e->{destapar(b1);});
-				gp.add(b1, k, k2);;
+				gp.add(b1, k2, k);;
 			}
 		}
 		Button b2 = new Button("Destapar todo");
 		Button b3 = new Button("Dar Pista");
+		gp1.getChildren().add(b2);
+		gp1.getChildren().add(b3);
 		vbox.getChildren().add(gp);
+		vbox.getChildren().add(gp1);
 		root.getChildren().add(vbox);
 		Stage stage = new Stage();
 		Scene scene = new Scene(root);
--- BuscaminasSinTerminar/src/ui/Main.java
+++ BuscaminasSinTerminar/src/ui/Main.java
@@ -18,14 +18,25 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			VBox root = new VBox();
-			Button b1 = new Button("Principiante");
+			Button b1 = new Button("PRINCIPIANTE");
+			Button b2 = new Button("INTERMEDIO");
+			Button b3 = new Button("EXPERTO");
 			Font value = new Font(25);
 			b1.setFont(value);
-			b1.setOnAction(e->{
+//			b1.setOnAction(e->{
 				c1.level(b1);
-			});
-			
+//			});
+			b2.setFont(value);
+//			b2.setOnAction(e->{
+				c1.level(b2);
+//			});
+			b3.setFont(value);
+//			b3.setOnAction(e->{
+				c1.level(b3);
+//			});
 			root.getChildren().add(b1);
+			root.getChildren().add(b2);
+			root.getChildren().add(b3);
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
@@ -38,6 +49,8 @@ public class Main extends Application {
 	public static void main(String[] args) {
 		launch(args);
 	}
+	
+	
 	
 	
 }
